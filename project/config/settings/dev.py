de7ce0278d36e3d0cabdd61f6eacdb79d3ed3177
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-09 20:03:42 +0300

    feat: Add initial project files and configurations
    This commit adds the initial project files and configurations. It
    includes the following changes:
    - Added `.env.dev` and `.env.prod` files with environment variables
    – Created `base.html` template file
    – Added `wsgi.py` and `asgi.py` files for Django deployment
    – Created `docker-compose.yml` and `Dockerfile` for containerization
    – Added `dev.py` and `prod.py` settings files for development and
    production environments
    – Included `manage.py` file for Django management commands
    – Added `README.md` file with project description and requirements.
    
    These changes lay the foundation for the project and set up the
    necessary files and configurations for development and deployment.

---
+++ project/config/settings/dev.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'my-secret-key'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test_task_db',
+        'USER': 'django_test_task_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
