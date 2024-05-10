11087e19adf90f1ee2093daf76ab6ef263a988ba
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-10 19:23:35 +0300

    Update shop URLs to use 'shops' instead of 'shop'

--- web/apps/core/core_urls.py
+++ web/apps/core/core_urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 app_name = 'core'
 urlpatterns = [
-    path('shop/', include('web.apps.core.urls.shop_urls')),
+    path('shops/', include('apps.core.urls.shop_urls')),
 ]
