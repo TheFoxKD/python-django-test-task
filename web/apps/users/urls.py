84953ce6984553050bebbd9a3d1904c9284b594e
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-11 00:22:31 +0300

    chore: Add 'users' app URLs to main configuration

--- web/apps/users/urls.py
+++ web/apps/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from ..users import views
 
+app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
