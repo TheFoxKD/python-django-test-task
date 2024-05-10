d0fcdbf7a57f72066985aa4762e49fc9088a5ddd
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-10 12:20:56 +0300

    feat: Update ShopListView to use ShopService for retrieving shops
    This commit updates the ShopListView in the shop_views.py file to use
    the ShopService for retrieving shops. It injects the ShopService
    dependency into the view and modifies the get_queryset method to call
    the get_all_shops method of the ShopService. This change improves the
    separation of concerns and makes the code more modular and testable.

--- web/apps/core/views/shop_views.py
+++ web/apps/core/views/shop_views.py
@@ -1,8 +1,19 @@
 from django.views.generic import CreateView, ListView, UpdateView
 
+from web.apps.core.models import Shop
+from web.apps.core.repositories.shop_repositories import ShopRepository
+from web.apps.core.services.shop_services import ShopService
+
+# Dependency Injection of ShopService 🤣😆
+shop_service = ShopService(ShopRepository())
+
 
 class ShopListView(ListView):
-    pass
+    model = Shop
+    template_name = 'shop/shop_list.html'
+
+    def get_queryset(self):
+        return shop_service.get_all_shops()
 
 
 class ShopSearchView(ListView):
