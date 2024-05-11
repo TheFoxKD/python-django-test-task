b3010749947529e2584c882c184ffa37dd733a58
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-11 14:18:13 +0300

    feat: Add user signup and login functionality
    The code changes in this commit add the `SignUpView` and `LoginView`
    classes to handle user signup and login functionality. The `SignUpView`
    includes a form for user registration and redirects to the shop list
    page upon successful registration. The `LoginView` is currently empty
    and needs to be implemented.

--- web/apps/users/views.py
+++ web/apps/users/views.py
@@ -1,13 +1,25 @@
+from typing import override
+
+from django.contrib.auth import get_user_model, login
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView
 
 from .forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('core:shop:list')
+
+    @override
+    def form_valid(self, form: CustomUserCreationForm):
+        self.object: User = form.save()
+        login(self.request, self.object)
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class LoginView(FormView):
