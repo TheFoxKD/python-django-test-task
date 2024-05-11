c98cf9f5ed8c2dbd3ade80ca5c12b51e4f215220
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-11 14:17:44 +0300

    feat: Add email field to user registration form

--- web/apps/users/forms.py
+++ web/apps/users/forms.py
@@ -1,9 +1,25 @@
+from typing import override
+
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import CustomUser
+from .repository import UserRepository
+from .services import UserService
+
+user_service = UserService(UserRepository())
+
+User = get_user_model()
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = ('username', 'password1', 'password2')
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    @override
+    def save(self, commit=True):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password1')
+        user: User = user_service.register_user(username=username, email=email, password=password)
+        return user
