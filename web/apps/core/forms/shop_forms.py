f51855a3e1672e7729fdd03129c5b918fec96f0c
Denis Krishtopa <krishtopadenis@gmail.com>
2024-05-15 23:03:50 +0300

    refactor: Update ImageWithPreviewField to use ImageUrlPreviewField

--- web/apps/core/forms/shop_forms.py
+++ web/apps/core/forms/shop_forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
-from ..fields import ImageWithPreviewField
+from ..fields import ImageUrlPreviewField
 from ..models import Shop
 
 
 class CreateShopForm(forms.ModelForm):
+    image = ImageUrlPreviewField(required=False)
+
     class Meta:
         model = Shop
         fields = ['title', 'description', 'image']
-
-    image = ImageWithPreviewField()
