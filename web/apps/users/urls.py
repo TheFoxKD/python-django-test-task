059006635fcd38d7aaf750391bdcf0999aa37618
Denis (TheFoxDK) Krishtopa <krishtopadenis@gmail.com>
2024-05-28 22:40:19 +0300

    refactor: Update login view to use FormView
    This commit refactors the LoginView class in the users/views.py module
    to inherit from the FormView class instead of the View class. This
    change improves code organization and consistency by using the
    appropriate class for handling form submissions. It also ensures that
    the login view follows the established pattern for handling form-related
     logic in the project.

--- web/apps/users/urls.py
+++ web/apps/users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('utils/', include('web.apps.users.urls_utils'))
+    path('utils/', include('apps.users.utils.urls')),
 ]
