89cc10ab6b9885f4e13506e45798af69e467141c
Denis (TheFoxDK) Krishtopa <krishtopadenis@gmail.com>
2024-05-28 22:55:43 +0300

    feat: Update CreateShopForm to include image field
    This commit updates the CreateShopForm in the shop_forms.py file to
    include an image field. The image field is an optional field that allows
     users to provide an image URL for their shop. This addition enhances
     the functionality of the form by allowing users to include an image
     with their shop details.

--- web/apps/core/forms/shop_forms.py
+++ web/apps/core/forms/shop_forms.py
@@ -1,15 +1,35 @@
 from django import forms
 
-from ..fields import DescriptionTextareaWidget, ImageUrlPreviewWidget, TitleInputWidget
+from ..fields import DescriptionTextareaWidget, ImagePreviewWidget, ImageUrlPreviewField, TitleInputWidget
 from ..models import Shop
+from ..repositories.shop_repositories import ShopRepository
+from ..services.shop_services import ShopService
+from ..types import URL
+
+shop_service = ShopService(ShopRepository())
 
 
 class CreateShopForm(forms.ModelForm):
+    image = ImageUrlPreviewField(required=False)
+
     class Meta:
         model = Shop
-        fields = ['title', 'description', 'imageUrl']
+        fields = ['title', 'description', 'image']
         widgets = {
             'title': TitleInputWidget(),
             'description': DescriptionTextareaWidget(),
-            'imageUrl': ImageUrlPreviewWidget()
+            'image': ImagePreviewWidget()
         }
+
+    def create_shop(self):
+        title: str = self.cleaned_data.get('title')
+        description: str = self.cleaned_data.get('description')
+        image_url: URL = self.cleaned_data.get('image')
+
+        shop = shop_service.create_shop(
+            title=title,
+            description=description,
+            image_url=image_url
+        )
+
+        return shop
